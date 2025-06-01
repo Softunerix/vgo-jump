6d400c4b9f327d362e325e8ea17f0d20cb132335
b-istiak-s <istiakshovon001@gmail.com>
2025-06-01 20:19:15 +0600

    add Laravel root detection using artisan file
    Updated file opening to use Alacritty with absolute paths built from project root and package path. Improved README accordingly.

--- README.md
+++ README.md
@@ -21,8 +21,27 @@
 go build -o vgo-jump main.go utils.go filepath_utils.go
 ```
 
+## To use in VIM
+
+```bash
+cp vgo-jump /usr/local/bin/vgo-jump
+```
+
+In VIM:
+```vim
+:!vgo-jump %:p <line> <col>
+```
+Use this command in VIM to run `vgo-jump` on the current file at the specified line and column.
+
 ## Limitations
 
-- Only works with basic use statements
-
+- Only works with basic `use` statements
 - Assumes valid UTF-8 input files
+- Assumes you have Alacritty installed
+- Opens a new terminal instead of a new tab in VIM
+
+## Future modifications
+
+- [ ] Multiple languages support
+- [ ] Open a new tab instead of a terminal
+- [ ] Find a better way to get the project root path
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 )
 
@@ -12,21 +14,60 @@ func main() {
 		os.Exit(1)
 	}
 
-	filepath := os.Args[1]
+	filePath := os.Args[1]
 	lineNum, _ := strconv.Atoi(os.Args[2])
 	colNum, _ := strconv.Atoi(os.Args[3])
 
-	packageName, err := getWordAtPosition(filepath, lineNum, colNum)
+	packageName, err := getWordAtPosition(filePath, lineNum, colNum)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	packagePath, err := getPackagePath(filepath, packageName)
+	packagePath, err := getPackagePath(filePath, packageName)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(packagePath)
+	fmt.Println("Project root found at:", filePath, "with package path:", packagePath)
+
+	projectRoot, err := FindProjectRoot(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// Join project root with cleanPath to get absolute path to target file
+	finalPath := filepath.Join(projectRoot, packagePath)
+
+	fmt.Println("Opening file:", finalPath)
+	cmd := exec.Command("alacritty", "-e", "vim", finalPath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Failed to open in Vim:", err)
+		os.Exit(1)
+	}
+}
+
+// FindProjectRoot tries to find the root of your Laravel project by searching upwards for a marker (like composer.json)
+func FindProjectRoot(startPath string) (string, error) {
+	dir := filepath.Dir(startPath)
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "artisan")); err == nil {
+			return dir, nil // found root
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			break // reached filesystem root
+		}
+		dir = parent
+	}
+
+	return "", fmt.Errorf("project root not found")
 }
